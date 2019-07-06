390530abcbb9d05c3ebded564969887aec9e679f
Ashutosh Srivastav <srivastavashutosh80@gmail.com>
2019-07-06 13:34:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/adminlte'));
+app.use(express.static(__dirname + '/dist/public'));
 
 app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/adminlte/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/public/index.html'));
 )};
 
 app.listen(process.env.PORT || 8000);
